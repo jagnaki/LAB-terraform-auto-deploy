18e3a4d195ef67c4223cfbde401c36c5d0bb9093
jagnaki <148761226+jagnaki@users.noreply.github.com>
2026-06-16 15:10:28 +0200

    Update health endpoint status to 'healthy v3'

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class Operacja(BaseModel):
 
 @app.get("/health")
 def health():
-    return {"status": "healthy v2"}
+    return {"status": "healthy v3"}
 
 @app.post("/dodaj")
 def dodaj(op: Operacja):
